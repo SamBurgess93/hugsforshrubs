9900cd85f3175774de6d6b137451b1bd99e6959d
SamBurgess93 <samburgess456@gmail.com>
2022-01-21 13:13:53 +0000

    Removed secret key and set debug

--- hugsforshrubs/settings.py
+++ hugsforshrubs/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b+7mctyea@uq^%!z(#lyhgu5k!l9r4a3a=#hbd47j+krcgf-22'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hugs-for-shrubs.herokuapp.com', 'localhost']
 
