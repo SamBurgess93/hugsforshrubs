7e8639df8904320937784ad84ffb409392c8a0c1
SamBurgess93 <samburgess456@gmail.com>
2022-01-28 15:24:08 +0000

    added delete review function

--- products/urls.py
+++ products/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.all_reviews, name='reviews'),
     path('add_review/<int:product_id>/', views.add_review, name='add_review'),
     path('edit_review/<int:review_id>/', views.edit_review, name='edit_review'),
+    path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
